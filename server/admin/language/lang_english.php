5aeb2193c9c78e825748b32c36b8576809005ab1
Xusqui <xusqui@gmail.com>
2017-07-08 16:22:26 +0200

    Create lang_english.php

---
+++ server/admin/language/lang_english.php
@@ -0,0 +1,115 @@
+<?php
+/***************************************************************************
+ *                             lang_english.php
+ *                            ------------------------
+ *		Version			: 1.0.0
+ *		Email			: xusqui@gmail.com
+ *		Site			: http://fernandezvillatoro.es
+ *		Copyright		: Xusqui. 2017
+ *
+ ***************************************************************************/
+
+#==== Generals
+$lang['activated1'] = 'Enabled:'; #Female for activated
+$lang['activated2'] = 'Enabled:'; #Male for activated
+$lang['update_config'] = 'Update configuration';
+$lang['default_config'] = 'Preset values';
+$lang['update'] = 'Update';
+$lang['ver'] = 'Client version:';
+$lang['pud'] = 'pud:';
+$lang['pin'] = 'GPIO:';
+$lang['debounce'] = 'Debounce (ms):';
+$lang['level'] = 'Level:';
+$lang['upload'] = 'Upload';
+$lang['upload_app'] = 'Upload client app';
+$lang['activate_destructive'] = 'Enable destructive buttons';
+$lang['manage_db'] = 'Manage BD and Logs';
+$lang['purge'] = 'ERASE data and logs prior to';
+$lang['days'] = 'days';
+$lang['erase'] = 'ERASE';
+$lang['reset_msg'] = 'ERASE EVERYTHING but config';
+$lang['reset'] = 'RESET';
+$lang['language'] = 'Language:';
+$lang['interface'] = 'Intarface';
+$lang['modem'] = 'Huawei Modem:';
+$lang['connection'] = 'Internet Connection';
+#====
+
+#==== Camera
+$lang['camera'] = 'Camera';
+$lang['hor_res'] = 'Horizontal resolution:';
+$lang['ver_res'] = 'Vertical resolution:';
+#====
+
+#==== Barometric Pressure
+$lang['baro'] = 'Barometric sensor';
+$lang['cs'] = 'GPIO CS:';
+$lang['sck'] = 'GPIO SCK:';
+$lang['sdi'] = 'GPIO SDI:';
+$lang['sdo'] = 'GPIO SDO:';
+#====
+
+#==== Temp / Hum Sensor
+$lang['temp'] = 'Tem/Hum Sensor';
+$lang['sensor'] = 'Kind of sensor:';
+$lang['retries'] = 'Retries:';
+#====
+
+#==== Rain gauge
+$lang['gauge'] = 'Rain gauge';
+$lang['wide'] = 'Wide (mm):';
+$lang['long'] = 'Long (mm):';
+$lang['drops'] = 'Drops (number):';
+#====
+
+#==== Anemometer
+$lang['anemometer'] = 'Anemometer';
+$lang['edges_rev'] = 'Edges/revolution:';
+$lang['hsf'] = 'High speed factor:';
+$lang['lsf'] = 'Low speed factor:';
+$lang['max_rot'] = 'Max rotation (sec):';
+$lang['diameter'] = 'Diameter (mm):';
+#====
+
+#==== Wind Vane
+$lang['vane'] = 'Wind vane';
+#====
+
+#==== Working mode
+$lang['working'] = 'Working mode';
+$lang['mode'] = 'Working mode:';
+#====
+
+#==== Raspberry config
+$lang['rasp_config'] = 'Raspberry config';
+$lang['temp_shtdwn'] = 'Temp to shutdown:';
+$lang['http_req'] = 'http request:';
+$lang['time_shtdwn'] = 'Time to shutdown:';
+$lang['update_every'] = 'Update every:';
+$lang['max_upload'] = 'Max upload (Kb):';
+#====
+
+#==== Client logs
+$lang['client_log'] = 'Client logs';
+$lang['backups'] = 'Backup numbers:';
+$lang['log_max'] = 'Max log size (Kb):';
+#====
+
+#==== Server logs
+$lang['server_log'] = 'Server logs';
+#====
+
+#==== Server outer connections
+$lang['server_ext'] = 'External Connections';
+$lang['server_meteotemplate'] = 'MeteoTemplate';
+$lang['server_meteotemplate_API'] = 'API URL:';
+$lang['server_meteotemplate_explanation'] = 'Exact URL to MeteoTemplate API';
+$lang['server_meteotemplate_secret'] = 'Password:';
+$lang['server_wunderground'] = 'WUnderground';
+$lang['server_wunderground_pass'] = 'Contraseña de WU:';
+$lang['server_wunderground_url'] = 'WU API URL:';
+$lang['server_wunderground_station'] = 'Station ID:';
+#====
+
+#==== Copyright
+$lang['copy'] = '&copy; 2017 Xisco Fernández - <a href="http://fernandezvillatoro.es">FernandezVillatoro.es</a> - <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPL v3.</a> Based on the great work by &copy; 2016 J&ouml;rg Zieren - <a href="http://zieren.de">zieren.de</a> - GNU GPL v3. Components: <a href="http://codefury.net/projects/klogger/">KLogger</a> by Kenny Katzgrau, MIT license. <a href="http://spin.js.org/">spin.js</a> (Not in this place) by Felix Gnass, Timothy Gu and others, MIT license.';
