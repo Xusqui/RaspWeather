031bc67314bb32bb077ef66b990e178a406840d3
Xusqui <xusqui@gmail.com>
2017-07-08 16:22:38 +0200

    Add files via upload

---
+++ server/admin/language/lang_spanish.php
@@ -0,0 +1,115 @@
+<?php
+/***************************************************************************
+ *                             lang_spanish.php
+ *                            ------------------------
+ *		Version			: 1.0.0
+ *		Email			: xusqui@gmail.com
+ *		Site			: http://fernandezvillatoro.es
+ *		Copyright		: Xusqui. 2017
+ *
+ ***************************************************************************/
+
+#==== Generals
+$lang['activated1'] = 'Activada:'; #Female for activated
+$lang['activated2'] = 'Activado:'; #Male for activated
+$lang['update_config'] = 'Actualizar configuración';
+$lang['default_config'] = 'Valores predeterminados';
+$lang['update'] = 'Actualizar';
+$lang['ver'] = 'Versión Cliente:';
+$lang['pud'] = 'pud:';
+$lang['pin'] = 'GPIO:';
+$lang['debounce'] = 'Debounce (ms):';
+$lang['level'] = 'Nivel:';
+$lang['upload'] = 'Subir';
+$lang['upload_app'] = 'Subir Aplicación Cliente';
+$lang['activate_destructive'] = 'Activar funciones de borrado';
+$lang['manage_db'] = 'Gestión de la Base de Datos y Logs';
+$lang['purge'] = 'BORRAR datos y logs anteriores a';
+$lang['days'] = 'días';
+$lang['erase'] = 'BORRAR';
+$lang['reset_msg'] = 'BORRARLO TODO EXCEPTO la configuración';
+$lang['reset'] = 'RESET';
+$lang['language'] = 'Idioma:';
+$lang['interface'] = 'Interfaz';
+$lang['modem'] = 'Módem Huawei:';
+$lang['connection'] = 'Conexión a Internet';
+#====
+
+#==== Camera
+$lang['camera'] = 'Cámara';
+$lang['hor_res'] = 'Resolución Horizontal:';
+$lang['ver_res'] = 'Resolución Vertical:';
+#====
+
+#==== Barometric Pressure
+$lang['baro'] = 'Sensor Barométrico';
+$lang['cs'] = 'GPIO CS:';
+$lang['sck'] = 'GPIO SCK:';
+$lang['sdi'] = 'GPIO SDI:';
+$lang['sdo'] = 'GPIO SDO:';
+#====
+
+#==== Temp / Hum Sensor
+$lang['temp'] = 'Sensor de Temperatura';
+$lang['sensor'] = 'Tipo de Sensor:';
+$lang['retries'] = 'Reintentos:';
+#====
+
+#==== Rain gauge
+$lang['gauge'] = 'Pluviómetro';
+$lang['wide'] = 'Ancho (mm):';
+$lang['long'] = 'Largo (mm):';
+$lang['drops'] = 'Gotas (número):';
+#====
+
+#==== Anemometer
+$lang['anemometer'] = 'Anemómetro';
+$lang['edges_rev'] = 'Colisiones/vuelta:';
+$lang['hsf'] = 'Factor a elevada velocidad:';
+$lang['lsf'] = 'Factor a baja velocidad:';
+$lang['max_rot'] = 'Rotación máxima (seg):';
+$lang['diameter'] = 'Diámetro (mm):';
+#====
+
+#==== Wind Vane
+$lang['vane'] = 'Veleta';
+#====
+
+#==== Working mode
+$lang['working'] = 'Modo de funcionamiento';
+$lang['mode'] = 'Modo de funcionamiento:';
+#====
+
+#==== Raspberry config
+$lang['rasp_config'] = 'Config Raspberry';
+$lang['temp_shtdwn'] = 'Temperatura de apagado:';
+$lang['http_req'] = 'Tpo. espera http request:';
+$lang['time_shtdwn'] = 'Tiempo de apagado:';
+$lang['update_every'] = 'Actualizar cada:';
+$lang['max_upload'] = 'Tamaño máx subida (Kb):';
+#====
+
+#==== Client logs
+$lang['client_log'] = 'Logs del Cliente';
+$lang['backups'] = 'Número de backups:';
+$lang['log_max'] = 'Tamaño máximo del log (Kb):';
+#====
+
+#==== Server logs
+$lang['server_log'] = 'Logs del Servidor';
+#====
+
+#==== Server outer connections
+$lang['server_ext'] = 'Conexiones externas';
+$lang['server_meteotemplate'] = 'MeteoTemplate';
+$lang['server_meteotemplate_API'] = 'URL de la API:';
+$lang['server_meteotemplate_explanation'] = 'Intruduce la URL exacta a la API de meteotemplate';
+$lang['server_meteotemplate_secret'] = 'Contraseña:';
+$lang['server_wunderground'] = 'WUnderground';
+$lang['server_wunderground_pass'] = 'Contraseña:';
+$lang['server_wunderground_url'] = 'WU API URL:';
+$lang['server_wunderground_station'] = 'ID Estación:';
+#====
+
+#==== Copyright
+$lang['copy'] = '&copy; 2017 Xisco Fernández - <a href="http://fernandezvillatoro.es">FernandezVillatoro.es</a> - <a href="https://www.gnu.org/licenses/gpl-3.0.en.html">GNU GPL v3.</a> Basado en el trabajo de &copy; 2016 J&ouml;rg Zieren - <a href="http://zieren.de">zieren.de</a> - GNU GPL v3. Components: <a href="http://codefury.net/projects/klogger/">KLogger</a> by Kenny Katzgrau, MIT license. <a href="http://spin.js.org/">spin.js</a> (No utilizado en este sitio) by Felix Gnass, Timothy Gu and others, MIT license.';
